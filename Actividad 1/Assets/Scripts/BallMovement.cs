e12098bffa2aa243a9525ff3691d1e3ce930d4a4
Andres <andres-salas@hotmail.com>
2019-09-11 12:23:58 -0500

    Tareas
    Tarea 1 completa
    Tarea 2 pendiente

---
+++ Actividad 1/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallMovement : MonoBehaviour {
+	private int score;
+    public Text countText;
+    public Rigidbody player;
+
+	// Start is called before the first frame update
+	void Start() {
+        SetScoreText();
+        player = GetComponent<Rigidbody>();
+    }
+
+	// Update is called once per frame
+	void Update() {
+		float h = Input.GetAxis("Horizontal"),
+		      v = Input.GetAxis("Vertical");
+		transform.Translate(
+			h * Time.deltaTime * 10,
+			v * Time.deltaTime * 10,
+			0,
+			Space.World
+			);
+	}
+
+	void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.CompareTag("Finish")) {
+            score += 1;
+            SetScoreText();
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            score -= 1;
+            SetScoreText();
+        }
+    }
+
+    void resetPosition() {
+        player.transform.position = new Vector3(0,-8,0);
+    }
+
+    void SetScoreText() {
+        countText.text = "Score: " + score.ToString();
+        resetPosition();
+    }
+}
---
+++ Actividad 1/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public Rigidbody enemy;
+    private int direction = 1;
+    // Start is called before the first frame update
+    void Start() {
+        enemy = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+         enemy.transform.Translate(new Vector3(direction * 4 *Time.deltaTime, 0.0f, 0.0f), Space.World);
+
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        direction *= -1;
+    }
+}
